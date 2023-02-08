be07a649f9090cffe7cfd9b1e6939513aa4fc8a0
Yakov Borevich <j.borevich@gmail.com>
2023-02-08 13:43:17 +0100

    Remove macro/target folder

--- bevy_promise_macro/target/debug/deps/bevy_promise_macro-bbb65864e40e7876.d
+++
@@ -1,5 +0,0 @@
-/Users/jkbo/proj/bevy_promise/bevy_promise_macro/target/debug/deps/bevy_promise_macro-bbb65864e40e7876.rmeta: src/main.rs
-
-/Users/jkbo/proj/bevy_promise/bevy_promise_macro/target/debug/deps/bevy_promise_macro-bbb65864e40e7876.d: src/main.rs
-
-src/main.rs:
